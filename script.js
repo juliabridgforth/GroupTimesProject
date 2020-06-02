f5cf9c639c99b968425d95a54294ab18a7b18fb8
LexTheMenace <64288254+LexTheMenace@users.noreply.github.com>
2020-06-02 10:15:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$("button").on("click", function() {
+   // var search = // Value of search form, Convert to string?
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=yourkey";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+      });
+  });
